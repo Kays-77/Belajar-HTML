264d9347ea50e5be6889275aafb4189b4a905270
Dinosaurus <skyskyee17@gmail.com>
2026-09-12 12:54:07 +0700

    update website

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("js jalan woi"); 
+const dropdown = document.querySelector(".dropdown");
+const button = dropdown.querySelector(".dropbtn");
+
+button.addEventListener("click", function () {
+    dropdown.classList.toggle("dropdown-show");
+});
